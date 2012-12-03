5032661573de6f7dadac62a4643a88ccbf24cddf
dmocontact <dmocontact@gmail.com>
2012-12-03 22:39:22 +0100

    add lot of files

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="fr">
 	<head>
-		<title>MONNIER Dorian ePortfolio</title>
+		<title>Dorian MONNIER ePortfolio</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link type="text/css" rel="stylesheet" href="css/main.css" media="all" />
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
@@ -14,12 +14,12 @@
 			<div id="headerContent">
 				<div id="logo">
 					<h1>
-						<a id="titlePortfolio" href="http://monnierdorian.free.fr">MONNIER Dorian</a>
+						<a id="titlePortfolio" href="http://monnierdorian.free.fr">Dorian MONNIER</a>
 					</h1>
 				</div>
 				<div class="menu_top">
 					<div class="element gold">
-						<a class="title" href="" title="Accéder à la page d'accueil du portfolio de MONNIER Dorian">Accueil</a>
+						<a class="title" href="" title="Accéder à la page d'accueil du portfolio de Dorian MONNIER">Accueil</a>
 						<span class="subtitle">Home</span>
 					</div>
 					<div class="element blue">
@@ -27,15 +27,15 @@
 						<span class="subtitle">Ma présentation</span>
 					</div>
 					<div class="element orange">
-						<a class="title" href="" title="Accéder aux Réalisations de MONNIER Dorian">Projets</a>
+						<a class="title" href="" title="Accéder aux Réalisations de Dorian MONNIER">Projets</a>
 						<span class="subtitle">Mes réalisations</span>
 					</div>
 					<div class="element red">
-						<a class="title" href="" title="Contacter MONNIER Doria">Contact</a>
+						<a class="title" href="" title="Contacter Dorian MONNIER">Contact</a>
 						<span class="subtitle">Me contacter</span>
 					</div>
 					<div class="element green">
-						<a class="title" href="" title="Accéder au blog de MONNIER Doria">Social</a>
+						<a class="title" href="" title="Accéder au blog de Dorian MONNIER">Social</a>
 						<span class="subtitle">News</span>
 					</div>
 				</div>
@@ -45,7 +45,8 @@
 				<div id="presentation">
 					<h2 class="blue">Présentation</h2>
 					<div class="presentation-cube l">
-						En construction.
+						Bienvenue sur mon e-portefolio. Je m'appelle Dorian Monnier, j'ai 24 ans et je suis en cinquième année du cycle d'ingénieur à l'EPSI Montpellier. 
+Sur ce site, vous pourrez obtenir plus d'informations sur mon parcours et mon projet personnel, ainsi que sur mes compétences en consultant mon CV et mes réalisations. Si mon profil vous intéresse et que vous souhaitez obtenir de plus amples informations, n'hésitez pas à me contacter.
 					</div>
 					<div class="presentation-cube r"></div>
 					<div class="hr"></div>
@@ -53,23 +54,10 @@
 				<div id="presentation">
 					<h2 class="orange">Projets</h2>
 					<div class="presentation-cube l">
-						En construction.
-					</div>
-					<div class="presentation-cube r"></div>
-					<div class="hr"></div>
-				</div>
-				<div id="presentation">
-					<h2 class="red">Contact</h2>
-					<div class="presentation-cube l">
-						En construction.
-					</div>
-					<div class="presentation-cube r"></div>
-					<div class="hr"></div>
-				</div>
-				<div id="presentation">
-					<h2 class="green">Social</h2>
-					<div class="presentation-cube l">
-						En construction.
+						Quels sont mes projet ?
+						<br />
+Je souhaite dans un premier temps acquérir de l'expérience au sein d'une start-up ou d'une petite entreprise pour suivre le projet au plus près. Ceci me permettra alors de partir à l'étranger pour découvrir une autre culture, monter en compétences et vivre de nouvelles expériences.
+J'aime relever de nouveaux défis et acquérir de nouvelles compétences.
 					</div>
 					<div class="presentation-cube r"></div>
 					<div class="hr"></div>
@@ -78,24 +66,24 @@
 		<div id="copyright">
 			<div class="menu_top">
 				<div class="element gold">
-					<span class="subtitle"><a class="title footer-link" href="homepage.php" title="Accéder à la page d'accueil du portfolio de MONNIER Dorian">Accueil</a></span>
+					<span class="subtitle"><a class="title footer-link" href="homepage.php" title="Accéder à la page d'accueil du portfolio de Dorian MONNIER">Accueil</a></span>
 				</div>
 				<div class="element blue">
 					<span class="subtitle"><a class="title footer-link" href="" title="Mieux me connaître">Profil</a></span>
 				</div>
 				<div class="element orange">
-					<span class="subtitle"><a class="title footer-link" href="" title="Accéder aux Réalisations de MONNIER Dorian">Projets</a></span>
+					<span class="subtitle"><a class="title footer-link" href="" title="Accéder aux Réalisations de Dorian MONNIER">Projets</a></span>
 				</div>
 				<div class="element red">
-					<span class="subtitle"><a class="title footer-link" href="" title="Contacter MONNIER Doria">Contact</a></span>
+					<span class="subtitle"><a class="title footer-link" href="" title="Contacter Dorian MONNIER">Contact</a></span>
 				</div>
 				<div class="element green">
-					<span class="subtitle"><a class="title footer-link" href="" title="Accéder au blog de MONNIER Doria">Social</a></span>
+					<span class="subtitle"><a class="title footer-link" href="" title="Accéder au blog de Dorian MONNIER">Social</a></span>
 				</div>
 			</div>
 			<span>
 				Copyright © 2010
-				<a title="MONNIER Dorian" alt="MONNIER Dorian" href="http://monnierdorian.free.fr">MONNIER Dorian</a></span>
+				<a title="Dorian MONNIER" alt="MONNIER Dorian" href="http://monnierdorian.free.fr">Dorian MONNIER</a></span>
 				Inc. Tout droits réservés
 			</span>
 		</div>
